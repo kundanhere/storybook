dfb114fe9e6743728db4f620946c419d563d6b59
Kundan Gupta <kundankumar8377@gmail.com>
2021-01-10 19:17:17 +0530

    handle story DELETE request

--- routes/stories.js
+++ routes/stories.js
@@ -40,35 +40,57 @@ router.get('/', ensureAuth, async (req, res) => {
 // @desc Show story edit page
 // @route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-  const story = await Story.findOne({ _id: req.params.id }).lean();
-  if (!story) {
-    return res.render('errors/404');
-  }
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean();
+    if (!story) {
+      return res.render('errors/404');
+    }
 
-  if (story.user != req.user.id) {
-    res.redirect('/stories');
-  } else {
-    res.render('stories/edit', { story });
+    if (story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      res.render('stories/edit', { story });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render('errors/500');
   }
 });
 
 // @desc Update story
 // @route PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean();
-  if (!story) {
-    return res.render('errors/404');
+  try {
+    let story = await Story.findById(req.params.id).lean();
+    if (!story) {
+      return res.render('errors/404');
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.redirect('/dashboard');
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render('errors/500');
   }
+});
 
-  if (story.user != req.user.id) {
-    res.redirect('/stories');
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
+// @desc Delete story
+// @route DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
     res.redirect('/dashboard');
+  } catch (err) {
+    console.error(err);
+    return res.render('errors/500');
   }
 });
 
